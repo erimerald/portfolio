3046b2dd12be07192b494e4a617741821d353e8b
erimerald <erimerald@gmail.com>
2022-03-10 21:45:47 +0200

    Set up css links and amended social security quiz description.

--- scripts/b_scripts_oliot.js
+++ scripts/b_scripts_oliot.js
@@ -8,7 +8,6 @@
 // Hakutoiminto elokuvatehtävässä kaipaa pientä tarkennusta.
 // Tulet tarvitsemaan tätä viimeisessä osiossa, joten kertaa vielä se, miten saat ohjelman tulostamaan "Ei löydy". Tätä voit pohtia lisähaasteena.
 
-
 // Sijoittaa kursorin input-laatikkoon ja muuttaa sen arvon tyhjäksi
 function alusta() {
   // Tyhjentää input-kenttien arvon
@@ -19,7 +18,7 @@ function alusta() {
   document.getElementById("inputHaeKohdenumero").value = "";
   // Vie hiiren ylimpään input-kenttään
   document.getElementById("inputKohdenumero").focus();
-;}
+}
 
 //luo asunnot-nimisen arrayn, johon asunto-objektit tallennetaan
 let asunnot = [];
@@ -28,78 +27,88 @@ let asunnot = [];
 // clean up all evidence of previous laskeNeliohinta print attempts
 // restyle css
 
-  //konstructori, joka luo asunto-nimisen objektin, joka tallentaa tiedot nimipaikoilleen 
-  // parseInt:in käyttö ei tässä kohtaa taida olla tarpeellista, mutta en ole varma joten käytin niitä tässäkin
+//konstructori, joka luo asunto-nimisen objektin, joka tallentaa tiedot nimipaikoilleen
+// parseInt:in käyttö ei tässä kohtaa taida olla tarpeellista, mutta en ole varma joten käytin niitä tässäkin
 function asunto(kohdenumero, osoite, hinta, pintaAla, laskeNelioHinta) {
   this.kohdenumero = parseInt(kohdenumero);
   this.osoite = osoite;
   this.hinta = parseInt(hinta);
   this.pintaAla = parseInt(pintaAla);
   // metodi laskee asunnon neliöhinnan
-  this.laskeNelioHinta = function(){
-    return hinta / pintaAla;}
-  }
+  this.laskeNelioHinta = function () {
+    return hinta / pintaAla;
+  };
+}
 
-  // luo asuntokohteen input-kenttien tiedoilla uuteen asunto-objektiin ja lisää nämä tiedot asunnot-arrayhin, kun asunnon tiedot on lisätty lomakkeelle
-  // kutsutaan Lisää asunto-nappia painettaessa
-function lisaaAsunto(){
-  // hakee muuttujat input-kentistä ja muuttaa numerot int-muotoon, luo myös laskeNeliöhinnalle oman paikan muuttujien listalla 
+// luo asuntokohteen input-kenttien tiedoilla uuteen asunto-objektiin ja lisää nämä tiedot asunnot-arrayhin, kun asunnon tiedot on lisätty lomakkeelle
+// kutsutaan Lisää asunto-nappia painettaessa
+function lisaaAsunto() {
+  // hakee muuttujat input-kentistä ja muuttaa numerot int-muotoon, luo myös laskeNeliöhinnalle oman paikan muuttujien listalla
   let kohdenumero = parseInt(document.getElementById("inputKohdenumero").value);
   let osoite = document.getElementById("inputOsoite").value;
   let hinta = parseInt(document.getElementById("inputHinta").value);
   let pintaAla = parseInt(document.getElementById("inputPintaAla").value);
   let laskeNelioHinta = "";
 
-   // tarkistaa ovatko kohteen lisäyskentät tyhjiä
+  // tarkistaa ovatko kohteen lisäyskentät tyhjiä
   if (!(kohdenumero > 0 && osoite.length > 0 && hinta > 0 && pintaAla > 0)) {
-    tulostus = "Täytä kohteen tiedot!"}
-
-    else if(asunnot.some(asunto => asunto.kohdenumero == kohdenumero)){
-    tulostus = "Kohde löytyy jo tietokannasta"
-    }
-
-else{
-    // luo uuden asunto-objektin 
-let uusiAsunto = new asunto(kohdenumero, osoite, hinta, pintaAla, laskeNelioHinta);
+    tulostus = "Täytä kohteen tiedot!";
+  } else if (asunnot.some((asunto) => asunto.kohdenumero == kohdenumero)) {
+    tulostus = "Kohde löytyy jo tietokannasta";
+  } else {
+    // luo uuden asunto-objektin
+    let uusiAsunto = new asunto(
+      kohdenumero,
+      osoite,
+      hinta,
+      pintaAla,
+      laskeNelioHinta
+    );
     // lisää uuden asunto-objektin asunnot-massiiviin
-asunnot.push(uusiAsunto);
-    // kertoo käyttäjälle, onnistuiko lisäys 
-tulostus = "Asunto on lisätty listaan!";
+    asunnot.push(uusiAsunto);
+    // kertoo käyttäjälle, onnistuiko lisäys
+    tulostus = "Asunto on lisätty listaan!";
     // tulostaa yksittäisen asunto-objektin ja asunnot-massiivin tiedot konsoliin
-console.log(asunto);
-console.log(asunnot);
-}
-    // muuttaa tulostusAlueen arvon tulostuksen arvoksi.
-document.getElementById("tulostusAlue").innerHTML = tulostus;
-    // tyhjentää input-kenttien arvot
-alusta();
+    console.log(asunto);
+    console.log(asunnot);
+  }
+  // muuttaa tulostusAlueen arvon tulostuksen arvoksi.
+  document.getElementById("tulostusAlue").innerHTML = tulostus;
+  // tyhjentää input-kenttien arvot
+  alusta();
 }
 
-    // hakee asunto-objktia asunnot massiivista kohdenumerolla, jonka käyttäjä lisää kohdenumeron inputtiin
-function haeAsunto(){
-    // tarkistaa toimiiko haku
+// hakee asunto-objktia asunnot massiivista kohdenumerolla, jonka käyttäjä lisää kohdenumeron inputtiin
+function haeAsunto() {
+  // tarkistaa toimiiko haku
   console.log("Haetaan kohdetta!");
   let haeKohdenumero = document.getElementById("inputHaeKohdenumero").value;
-    // lisää inputin kohdenumeron konsoliin tarkistusta varten 
+  // lisää inputin kohdenumeron konsoliin tarkistusta varten
   console.log(haeKohdenumero);
-    // tarkistaa onko kohdenumeron kenttä tyhjä
+  // tarkistaa onko kohdenumeron kenttä tyhjä
   if (haeKohdenumero.length == 0) {
-    tulostus = "Kirjoita kohdenumero!"
+    tulostus = "Kirjoita kohdenumero!";
   }
-    // jos kohdenumeron kenttä ei ole tyhjä, tarkistaa löytyykö asunto-objektia asunnot-massiivista objektin kohdenumero-arvolla 
-else if(asunnot.some(asunto => asunto.kohdenumero == haeKohdenumero)){
+  // jos kohdenumeron kenttä ei ole tyhjä, tarkistaa löytyykö asunto-objektia asunnot-massiivista objektin kohdenumero-arvolla
+  else if (asunnot.some((asunto) => asunto.kohdenumero == haeKohdenumero)) {
     // jos kohdenumero löytyi, hakee asunnot-massiivista kohdenumeron
-let haetunKohteenNumero = asunnot.find(item => item.kohdenumero == haeKohdenumero);
+    let haetunKohteenNumero = asunnot.find(
+      (item) => item.kohdenumero == haeKohdenumero
+    );
     // tulostaa löydetyn objektin kohdenumeron ja kutsuu objektin laskeNeliöhinta-metodia
-tulostus = "Haetun kohteen numero on " +haetunKohteenNumero.kohdenumero+", ja sen neliöhinta on " + haetunKohteenNumero.laskeNelioHinta() + " € / m2";
+    tulostus =
+      "Haetun kohteen numero on " +
+      haetunKohteenNumero.kohdenumero +
+      ", ja sen neliöhinta on " +
+      haetunKohteenNumero.laskeNelioHinta() +
+      " € / m2";
     // tyhjentää input-kentät
-alusta()}
-    // jos hakukohdetta ei löytynyt, antaa ilmoituksen virheestä
-   else
-  {
-      tulostus = "Kohdetta ei löytynyt";
-    }
-    // muuttaa tulostusAlueen arvon tulostuksen arvoksi.
-    document.getElementById("tulostusAlue").innerHTML = tulostus;
+    alusta();
   }
-
+  // jos hakukohdetta ei löytynyt, antaa ilmoituksen virheestä
+  else {
+    tulostus = "Kohdetta ei löytynyt";
+  }
+  // muuttaa tulostusAlueen arvon tulostuksen arvoksi.
+  document.getElementById("tulostusAlue").innerHTML = tulostus;
+}
