8dc0536e4b8b5041fb65193581dd11abd0cea25a
erimerald <erimerald@gmail.com>
2024-01-16 23:03:16 +0200

    Base of minesweeper

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -5,19 +5,57 @@ const endGameText = document.querySelector(".end-game-text");
 const playAgainButton = document.querySelector(".play-again");
 
 const totalCells = 100;
-const totalBombs = 90;
-
+const totalBombs = 20;
 // player wins when this score is reached
-maxScore = 5;
-
+maxScore = 10;
 // lists which cells bombs go in
-const bombsList = [16, 21, 3];
+const bombsList = [];
 
 let score = 0;
 
-const cell = document.createElement("div");
-cell.classList.add("cell");
+function updateScore() {
+  score++;
+  scoreCounter.innerText = score.toString().padStart(5, "0");
+  if (score === maxScore) {
+    endGame(true);
+  }
+}
 
-// for (let index = 1; index <100 =  ){
+for (let index = 1; index <= 100; index++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
 
-// }
+  cell.addEventListener("click", function () {
+    if (bombsList.includes(index)) {
+      cell.classList.add("cell-bomb");
+      endGame(false);
+    }
+
+    cell.classList.add("cell-clicked");
+    updateScore();
+    console.log(`You clicked on cell number ${index}`);
+  });
+  grid.appendChild(cell);
+}
+
+while (bombsList.length < totalBombs) {
+  // Generate a random number between 1-100, inclusive
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+  if (!bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
+  console.log(randomNumber);
+}
+
+function endGame(isVictory) {
+  if (isVictory) {
+    endGameText.innerHTML = "YOU <br> WON";
+    endGameScreen.classList.add("win");
+  }
+  endGameScreen.classList.remove("hidden");
+}
+console.log(bombsList);
+
+playAgainButton.addEventListener("click", function () {
+  window.location.reload();
+});
