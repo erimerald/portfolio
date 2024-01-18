620df19186d501fd3ab9b7aef7bea1d37ba3fab3
erimerald <erimerald@gmail.com>
2024-01-18 18:59:51 +0200

    Added empty cells left counter

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -3,11 +3,12 @@ const grid = document.querySelector(".grid");
 const endGameScreen = document.querySelector(".end-game-screen");
 const endGameText = document.querySelector(".end-game-text");
 const playAgainButton = document.querySelector(".play-again");
+const emptyCellsText = document.querySelector(".empty-cells-counter");
 
 const totalCells = 100;
 const totalBombs = 20;
 // player wins when this score is reached
-maxScore = 10;
+maxScore = (totalCells - totalBombs) / 2;
 // lists which cells bombs go in
 const bombsList = [];
 
@@ -16,6 +17,9 @@ let score = 0;
 function updateScore() {
   score++;
   scoreCounter.innerText = score.toString().padStart(5, "0");
+  // emptyCellsRemaining = maxScore - score;
+  emptyCellsText.innerText = (maxScore - score).toString();
+
   if (score === maxScore) {
     endGame(true);
   }
